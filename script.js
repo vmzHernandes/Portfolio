abb847db7ba4d7fed591856422c4e5322170866a
Vitor Marra Hernandes <v.marrahernandes@gmail.com>
2023-05-25 20:44:21 -0300

    third commit

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function stickyHeader() {
       header.classList.remove("sticky");
     }
   } 
+// Mancha
+const mancha = document.getElementById("mancha");
+document.body.onpointermove = event => {
+  const {clientX, clientY} = event;
+  mancha.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, {duration: 3000, fill: "forwards"});
+}
